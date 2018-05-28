16c7ae3f1c1a8f222d488d33b7b69eaa965d55e8
GuoxinQiu <guoxin.q@verifone.cn>
2018-05-28 18:00:18 +0800

    add Elevator domain.

---
+++ dom/src/main/java/elevatorapp/Elevators.java
@@ -0,0 +1,69 @@
+package elevatorapp;
+
+import java.util.List;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.Action;
+import org.apache.isis.applib.annotation.ActionLayout;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.NatureOfService;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import org.apache.isis.applib.annotation.SemanticsOf;
+import org.apache.isis.applib.services.eventbus.ActionDomainEvent;
+import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
+import org.apache.isis.applib.services.repository.RepositoryService;
+
+@DomainService(
+        nature = NatureOfService.VIEW_MENU_ONLY
+)
+@DomainServiceLayout(
+        named = "Elevators",
+        menuOrder = "11"
+)
+public class Elevators {
+
+    @ActionLayout(cssClassFa = "fa fa-plus")
+    @MemberOrder(sequence = "5")
+    public Elevator newElevator(
+            @Parameter(regexPattern = "\\w[@&:\\-\\,\\.\\+ \\w]*")
+            final String name){
+        final Elevator ele = container.newTransientInstance(Elevator.class);
+        ele.setName(name);
+
+        return ele;
+    }
+
+    /*
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
+    @MemberOrder(sequence = "1")
+    public List<Elevator> listAll() {
+        return repositoryService.allInstances(Elevator.class);
+    }
+
+
+    public static class CreateDomainEvent extends ActionDomainEvent<Elevators> {}
+    @Action(domainEvent = CreateDomainEvent.class)
+    @MemberOrder(sequence = "2")
+    public Elevator create(
+            @ParameterLayout(named="Name")
+            final String name) {
+        Elevator ae = new Elevator(name);
+        return ae;
+        //return repositoryService.persist(new Elevator(name));
+    }
+    */
+
+    @javax.inject.Inject
+    RepositoryService repositoryService;
+
+    @javax.inject.Inject
+    IsisJdoSupport isisJdoSupport;
+
+    @javax.inject.Inject
+    private DomainObjectContainer container;
+}
